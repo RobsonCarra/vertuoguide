739b04b9e7596d6f9a28cc6e966b3c9a970be855
Pierry Borges <pieerry@gmail.com>
2021-09-13 21:28:27 -0300

    Correção Coffee

--- app/src/main/java/br/com/alura/ceep/ui/coffemachine/helpers/RetrofitConfig.kt
+++ app/src/main/java/br/com/alura/ceep/ui/coffemachine/helpers/RetrofitConfig.kt
@@ -1,5 +1,6 @@
 package br.com.alura.ceep.ui.coffemachine.helpers
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,6 +8,7 @@ class RetrofitConfig {
     fun getClient(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://run.mocky.io/v3/")
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
